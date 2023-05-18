0e101baa830787d5e20fdc71e8057eb98af68a20
NurAthirah0 <s21b0044@siswa.umk.edu.my>
2023-05-18 12:04:01 +0800

    Merge remote-tracking branch 'origin/master'

--- lib/main.dart
+++ lib/main.dart
@@ -1,126 +1,33 @@
-import 'package:flutter/material.dart';
+void _login() {
+  String email = _emailController.text;
+  String password = _passwordController.text;
 
-void main() {
-  runApp(SpendingSavingApp());
-}
+  // Perform login validation and logic here
+  // You can integrate with authentication services or check credentials locally
 
-class SpendingSavingApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Spending & Saving App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: LoginPage(),
+  if (email.isNotEmpty && password.isNotEmpty) {
+    // Navigate to the main screen or dashboard
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => DashboardPage()),
     );
-  }
-}
-
-class LoginPage extends StatefulWidget {
-  @override
-  _LoginPageState createState() => _LoginPageState();
-}
-
-class _LoginPageState extends State<LoginPage> {
-  TextEditingController _emailController = TextEditingController();
-  TextEditingController _passwordController = TextEditingController();
-
-  void _login() {
-    String email = _emailController.text;
-    String password = _passwordController.text;
-
-    // Perform login validation and logic here
-    // You can integrate with authentication services or check credentials locally
-
-    if (email == 'example@example.com' && password == 'password') {
-      // Navigate to the main screen or dashboard
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => DashboardPage()),
-      );
-    }
-    else if void _login() {
-      String email = _emailController.text.trim();
-      String password = _passwordController.text.trim();
-
-      print('Entered email: $email');
-      print('Entered password: $password');
-
-      // Perform login validation and logic here
-      // Add debug statements or log messages to check the values being used for comparison
-
-      // Rest of the login logic...
-    }
-
-    else {
-      showDialog(
-        context: context,
-        builder: (context) {
-          return AlertDialog(
-            title: Text('Login Failed'),
-            content: Text('Invalid email or password.'),
-            actions: <Widget>[
-              TextButton(
-                child: Text('OK'),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-              ),
-            ],
-          );
-        },
-      );
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Login'),
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextField(
-              controller: _emailController,
-              decoration: InputDecoration(
-                labelText: 'Email',
-              ),
-            ),
-            SizedBox(height: 16.0),
-            TextField(
-              controller: _passwordController,
-              decoration: InputDecoration(
-                labelText: 'Password',
-              ),
-              obscureText: true,
-            ),
-            SizedBox(height: 16.0),
-            ElevatedButton(
-              onPressed: _login,
-              child: Text('Login'),
+  } else {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Login Failed'),
+          content: Text('Please enter both email and password.'),
+          actions: <Widget>[
+            TextButton(
+              child: Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
             ),
           ],
-        ),
-      ),
-    );
-  }
-}
-
-class DashboardPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Dashboard'),
-      ),
-      body: Center(
-        child: Text('Welcome to the Spending & Saving App!'),
-      ),
+        );
+      },
     );
   }
 }
